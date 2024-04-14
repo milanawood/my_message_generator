7100992fd3095fbe948ff4c0b5b8fc1e8831cba1
Milana Wood <milanawoodstars@Milanas-MacBook-Air.local>
2024-04-14 16:05:26 +0100

    Edit double typing of lucky number

--- main.js
+++ main.js
@@ -6,18 +6,7 @@ const returnRandFortune = () => {
     let spiritAnimal = ['a Tiger', 'a Rabbit', 'a Butterfly', 'a Dog', 'a Bear', 'a Cat', 'an Eagle', 'a Snake']
     let randSpiritAnimal = spiritAnimal[Math.floor(Math.random() * 8)]
     let randMessage = fortuneMessages[Math.floor(Math.random() * 5)]
-    return `${randMessage}! Your lucky number is ${fortuneNumbers}. Your spirit animal is ${randSpiritAnimal}.`
+    return `${randMessage}! ${fortuneNumbers}. Your spirit animal is ${randSpiritAnimal}.`
   }
 
   console.log(returnRandFortune());
-  
-  
-  /* Returns a random single stand of DNA containing 15 bases
-  const mockUpStrand = () => {
-    const newStrand = []
-    for (let i = 0; i < 15; i++) {
-      newStrand.push(returnRandBase())
-    }
-    return newStrand
-  } 
-  */
